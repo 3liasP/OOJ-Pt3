12d75bbd86aff6b4da3e703e57538777601b996c
Jari-Matti Mäkelä <jmjmak@utu.fi>
2022-10-20 21:18:06 +0300

    Julkistettu metodin näkyvyys (helpompi käyttää demoamiseen)

--- src/main/java/fi/utu/tech/ooj/exercise3/tehtava4ab/Eläin.java
+++ src/main/java/fi/utu/tech/ooj/exercise3/tehtava4ab/Eläin.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Eläin {
-    static <X extends Z, Y extends Z, Z extends Eläin> List<Z> yhdistä(List<X> xs, List<Y> ys) {
+    public static <X extends Z, Y extends Z, Z extends Eläin> List<Z> yhdistä(List<X> xs, List<Y> ys) {
         var tmp = new ArrayList<Z>();
         for (var x : xs) tmp.add(x);
         for (var y : ys) tmp.add(y);
