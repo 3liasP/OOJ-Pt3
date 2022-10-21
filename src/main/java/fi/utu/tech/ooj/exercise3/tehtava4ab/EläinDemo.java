c40df9fa7e9da1d098269c8e8afccd2fb4ac62de
Jari-Matti Mäkelä <jmjmak@utu.fi>
2022-10-21 17:05:39 +0300

    Julkistettu metodin näkyvyys (helpompi käyttää demoamiseen)

--- src/main/java/fi/utu/tech/ooj/exercise3/tehtava4ab/EläinDemo.java
+++ src/main/java/fi/utu/tech/ooj/exercise3/tehtava4ab/EläinDemo.java
@@ -3,7 +3,7 @@ package fi.utu.tech.ooj.exercise3.tehtava4ab;
 import java.util.ArrayList;
 
 public class EläinDemo {
-    static void demo() {
+    public static void demo() {
         ArrayList<Eläin> elukat = new ArrayList<Eläin>();
         ArrayList<Kissa> kissat = new ArrayList<Kissa>();
         ArrayList<Koira> koirat = new ArrayList<Koira>();
